e9433911121ed33e6e70f5da9405f0c6bc057bb0
Vaibhav Ranga <vaibhavranga2@gmail.com>
2024-04-13 15:12:31 +0530

    Initial Commit

---
+++ HWS-3-BetterRest/HWS_3_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_3_BetterRestApp.swift
+//  HWS-3-BetterRest
+//
+//  Created by Vaibhav Ranga on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HWS_3_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
